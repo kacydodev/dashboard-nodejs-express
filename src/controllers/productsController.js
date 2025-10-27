0e9d0f5a458cb79b34728eb24956e248489f0a7c
Kacy Do <contact@kacydo.dev>
2025-10-27 12:05:28 +1100

    add function for search query

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -21,11 +21,16 @@ export async function getProducts(req, res) {
     let query = 'SELECT * FROM products';
     let params = [];
 
-    const { genre } = req.query;
+    const { genre, search } = req.query;
 
     if (genre) {
       query += ' WHERE genre = ?';
       params.push(genre);
+    } else if (search) {
+      query += ' WHERE title LIKE ? OR artist LIKE ? OR genre LIKE ?';
+      const searchPattern = `%${search}%`;
+      params.push(searchPattern, searchPattern, searchPattern);
+      // ?search=lo
     }
 
     const products = await db.all(query, params);
